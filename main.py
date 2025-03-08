d11b1957a36988ba15104a1458def457ad81b597
Tuyen Nguyen <nguyenetuyen9201@gmail.com>
2025-03-08 22:16:29 +0700

    [FEAT] add requirement file and update code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, HTTPException
+from typing import Optional
 import os
 import pymysql
 import requests
@@ -14,8 +15,9 @@ REPO_NAME = os.getenv('REPO_NAME')
 
 # GITHUB API
 BASE_URL = os.getenv('BASE_URL')
+TOKEN = os.getenv('TOKEN')
 
-# DATABASE CONNECTION
+# DATABASE CONNECTION, USING MYSQL WORKBENCH
 DB_HOST = os.getenv('DB_HOST')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
@@ -38,7 +40,7 @@ def home():
     return "Welcome"
 
 @app.get("/commits")
-def read_root():
+def get_commits():
     # calculate the time of the since 6 month
     time = (datetime.utcnow() - timedelta(days=180)).isoformat()
     data_length = 100
@@ -51,21 +53,25 @@ def read_root():
     while data_length == 100:
         # run github endpoint to extract data
         url = f'{BASE_URL}/repos/{REPO_OWNER}/{REPO_NAME}/commits?since={time}&per_page=100&page={page}'
-        response = requests.get(url)
+        headers = {"Authorization": f"token {TOKEN}"}
+        response = requests.get(url, headers = headers)
         commits = response.json()
         data_length = len(commits)
 
         if (data_length == 0):
             break
 
+        # handle json response to store the data
         for commit in commits:
-            print(commit)
             sha = commit["sha"]
-            author = commit["author"]["login"]
+            author = None
+            if "author" in commit and commit["author"] and "login" in commit["author"]:
+                author = commit["author"]["login"]
             committer = commit["committer"]["id"]
             message = commit["commit"]["message"]
             commit_datetime = commit["commit"]["author"]["date"]
 
+            # change the datetime format for MySQL
             format_commit_datetime = datetime.strptime(commit_datetime, "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%d %H:%M:%S")
 
             cursor.execute("SELECT COUNT(*) FROM commits WHERE sha = %s", (sha))
@@ -73,35 +79,37 @@ def read_root():
                 cursor.execute("INSERT INTO commits (sha, committer, message, date, author) VALUES (%s, %s, %s, %s, %s)", (sha, committer, message, format_commit_datetime, author))
             else:
                 cursor.execute("UPDATE commits SET committer = %s, message = %s, date = %s, author = %s WHERE sha = %s", (committer, message, format_commit_datetime, author, sha))
-        page += 1
 
-    # save change in the database
-    conn.commit()
+        page += 1
+        # save change in the database
+        conn.commit()
+
     # close the cursor and the database connection
     cursor.close()
     conn.close()
 
     return commits
 
-@app.get("/committers/top5")
-def get_five_committers():
+@app.get("/committers")
+def get_top_committers(top: Optional[int] = Query(5, ge=1)):
     conn = connect_db()
     cursor = conn.cursor()
 
-    cursor.execute('''
-        SELECT author, COUNT(*) AS number_of_commits
-        FROM commits
-        GROUP BY author
-        ORDER BY number_of_commits DESC
-        LIMIT 5;
-    ''')
-    results = cursor.fetchall()
-
-    # save change in the database
-    conn.commit()
-    # close the cursor and the database connection
-    cursor.close()
-    conn.close()
+    try:
+        # select all record and group by author for counting number of commit
+        cursor.execute('''
+            SELECT author, COUNT(*) AS number_of_commits
+            FROM commits
+            GROUP BY author
+            ORDER BY number_of_commits DESC
+            LIMIT %s;''', (top))
+        results = cursor.fetchall()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        # close the cursor and the database connection
+        cursor.close()
+        conn.close()
 
     return results
 
@@ -110,35 +118,40 @@ def get_streak():
     conn = connect_db()
     cursor = conn.cursor()
 
-    cursor.execute('''
-        SELECT author, streak
-        FROM (
-            SELECT
-                author,
-                commit_date,
-                @previous_commit_date AS previous_commit_date,
-                @previous_author AS previous_author,
-                @streak := IF(@previous_author = author AND DATEDIFF(commit_date, @previous_commit_date) = 1, @streak + 1, 1) AS streak,
-                @max_streak := GREATEST(@max_streak, @streak) AS max_streak,
-                @previous_commit_date := commit_date,
-                @previous_author := author
+    try:
+        # 1. select authors with date of commit
+        # 2. calculate the streak: check if the previous commit date and date are continuously and the same author the increase streak by one, if not set streak to 1
+        # 3. remember the maximum of streak
+        # 4. select the authors with highest streak
+        cursor.execute('''
+            SELECT author, streak
             FROM (
-                SELECT DISTINCT author, DATE(date) AS commit_date
-                FROM etl_assessment.commits
-                ORDER BY author, commit_date
-                ) AS ordered_commits,
-                (SELECT @previous_commit_date := NULL, @previous_author := NULL, @streak := 0, @max_streak := 0) AS variables
-        ) AS streak_commit
-        WHERE streak = @max_streak
-        ORDER BY author;
-    ''')
-    results = cursor.fetchall()
-
-    # save change in the database
-    conn.commit()
-    # close the cursor and the database connection
-    cursor.close()
-    conn.close()
+                SELECT
+                    author,
+                    commit_date,
+                    @previous_commit_date AS previous_commit_date,
+                    @previous_author AS previous_author,
+                    @streak := IF(@previous_author = author AND DATEDIFF(commit_date, @previous_commit_date) = 1, @streak + 1, 1) AS streak,
+                    @max_streak := GREATEST(@max_streak, @streak) AS max_streak,
+                    @previous_commit_date := commit_date,
+                    @previous_author := author
+                FROM (
+                    SELECT DISTINCT author, DATE(date) AS commit_date
+                    FROM etl_assessment.commits
+                    ORDER BY author, commit_date
+                    ) AS ordered_commits,
+                    (SELECT @previous_commit_date := NULL, @previous_author := NULL, @streak := 0, @max_streak := 0) AS variables
+            ) AS streak_commit
+            WHERE streak = @max_streak
+            ORDER BY author;
+        ''')
+        results = cursor.fetchall()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        # close the cursor and the database connection
+        cursor.close()
+        conn.close()
 
     return results
 
@@ -147,27 +160,29 @@ def generate_heatmap():
     conn = connect_db()
     cursor = conn.cursor()
 
-    cursor.execute('''
-        SELECT
-            DAYNAME(date) AS day_of_week,
-            SUM(CASE WHEN HOUR(date) BETWEEN 1 AND 3 THEN 1 ELSE 0 END) AS '01-03',
-            SUM(CASE WHEN HOUR(date) BETWEEN 4 AND 6 THEN 1 ELSE 0 END) AS '04-06',
-            SUM(CASE WHEN HOUR(date) BETWEEN 7 AND 9 THEN 1 ELSE 0 END) AS '07-09',
-            SUM(CASE WHEN HOUR(date) BETWEEN 10 AND 12 THEN 1 ELSE 0 END) AS '10-12',
-            SUM(CASE WHEN HOUR(date) BETWEEN 13 AND 15 THEN 1 ELSE 0 END) AS '13-15',
-            SUM(CASE WHEN HOUR(date) BETWEEN 16 AND 18 THEN 1 ELSE 0 END) AS '16-18',
-            SUM(CASE WHEN HOUR(date) BETWEEN 19 AND 21 THEN 1 ELSE 0 END) AS '19-21',
-            SUM(CASE WHEN HOUR(date) >= 22 OR HOUR(date) = 0 THEN 1 ELSE 0 END) AS '22-00'
-        FROM etl_assessment.commits
-        GROUP BY day_of_week
-        ORDER BY FIELD(day_of_week, 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
-    ''')
-    results = cursor.fetchall()
-
-    # save change in the database
-    conn.commit()
-    # close the cursor and the database connection
-    cursor.close()
-    conn.close()
+    try:
+        # go through each record and check the hour, if hour's in one case then increase the total commit by one
+        cursor.execute('''
+            SELECT
+                DAYNAME(date) AS day_of_week,
+                SUM(CASE WHEN HOUR(date) BETWEEN 1 AND 3 THEN 1 ELSE 0 END) AS '01-03',
+                SUM(CASE WHEN HOUR(date) BETWEEN 4 AND 6 THEN 1 ELSE 0 END) AS '04-06',
+                SUM(CASE WHEN HOUR(date) BETWEEN 7 AND 9 THEN 1 ELSE 0 END) AS '07-09',
+                SUM(CASE WHEN HOUR(date) BETWEEN 10 AND 12 THEN 1 ELSE 0 END) AS '10-12',
+                SUM(CASE WHEN HOUR(date) BETWEEN 13 AND 15 THEN 1 ELSE 0 END) AS '13-15',
+                SUM(CASE WHEN HOUR(date) BETWEEN 16 AND 18 THEN 1 ELSE 0 END) AS '16-18',
+                SUM(CASE WHEN HOUR(date) BETWEEN 19 AND 21 THEN 1 ELSE 0 END) AS '19-21',
+                SUM(CASE WHEN HOUR(date) >= 22 OR HOUR(date) = 0 THEN 1 ELSE 0 END) AS '22-00'
+            FROM commits
+            GROUP BY day_of_week
+            ORDER BY FIELD(day_of_week, 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+        ''')
+        results = cursor.fetchall()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        # close the cursor and the database connection
+        cursor.close()
+        conn.close()
 
     return results
